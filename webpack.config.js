df3a161d5c1847bd51659ae936c400351db14e81
Xuanwo <xuanwo@yunify.com>
2017-11-19 02:18:31 +0800

    webpack: Add webpack config for node and browser
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const nodeConfig = {
+  entry: './index.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'node/qingstor-sdk.js',
+    libraryTarget: "umd",
+    library: "qingstor_sdk"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'fetch': './fetch/fetch-node'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+              "targets": {
+                "node": [
+                  "6"
+                ]
+              },
+              "useBuiltIn": true
+            }]],
+            plugins: [
+              "add-module-exports"
+            ]
+          }
+        }
+      },
+    ]
+  }
+};
+
+const browserConfig = {
+  entry: ['@babel/polyfill', './index.js'],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'browser/qingstor-sdk.js',
+    libraryTarget: "var",
+    library: "qingstor_sdk"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'fetch': './fetch/fetch-browser'
+    })
+  ],
+  node: {
+    "fs": "empty",
+    "process": "mock"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": {
+                  "browsers": [
+                    "> 1%"
+                  ],
+                  "loose": true,
+                  "modules": false
+                },
+                "useBuiltIn": true
+              }]
+            ],
+            "plugins": [
+              "add-module-exports"
+            ]
+          }
+        }
+      },
+    ]
+  }
+};
+
+module.exports = [
+  nodeConfig,
+  browserConfig,
+];
