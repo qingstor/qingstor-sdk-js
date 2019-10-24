ae36c0b399fdc30358949d7c69782758283d1b07
Mark <markduan@yunify.com>
2019-10-24 14:54:54 +0800

    test: Update test script excute command

---
+++ scenarios/tests/bucket.js
@@ -0,0 +1,130 @@
+import assert from 'assert';
+import { Given, When, Then } from 'cucumber';
+
+import Config from '../../src/config/browser';
+import QingStor from '../../src/qingstor/qingstor';
+
+When('initialize the bucket', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('the bucket is initialized', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('put bucket', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('put bucket status code is {int}', function(int) {
+  // Then('put bucket status code is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('put same bucket again', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('put same bucket again status code is {int}', function(int) {
+  // Then('put same bucket again status code is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('list objects', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('list objects status code is {int}', function(int) {
+  // Then('list objects status code is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('list objects keys count is {int}', function(int) {
+  // Then('list objects keys count is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('head bucket', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('head bucket status code is {int}', function(int) {
+  // Then('head bucket status code is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('delete multiple objects:', function(docString) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('delete multiple objects code is {int}', function(int) {
+  // Then('delete multiple objects code is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('get bucket statistics', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('get bucket statistics status code is {int}', function(int) {
+  // Then('get bucket statistics status code is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('get bucket statistics status is {string}', function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('delete bucket', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('delete bucket status code is {int}', function(int) {
+  // Then('delete bucket status code is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('an object created by initiate multipart upload', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('list multipart uploads', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('list multipart uploads count is {int}', function(int) {
+  // Then('list multipart uploads count is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('list multipart uploads with prefix', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('list multipart uploads with prefix count is {int}', function(int) {
+  // Then('list multipart uploads with prefix count is {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
