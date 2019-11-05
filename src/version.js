aebcf451ef2acc6c23d7fdd34f0844d3e6cb5113
Mark <markduan@yunify.com>
2019-11-05 11:10:14 +0800

    Bump version to 3.0.0-beta.6

--- src/version.js
+++ src/version.js
@@ -14,4 +14,4 @@
 // | limitations under the License.
 // +-------------------------------------------------------------------------
 
-export default '3.0.0-beta.5';
+export default '3.0.0-beta.6';
